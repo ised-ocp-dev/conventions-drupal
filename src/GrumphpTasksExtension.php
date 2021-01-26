d4bf21587b7c4187045d2240c5724826fa5d097c
william roboly <william.roboly@appnovation.com>
2021-01-26 12:03:49 -0500

    Updates to clean up the drupal conventions.

--- src/GrumphpTasksExtension.php
+++ src/GrumphpTasksExtension.php
@@ -17,36 +17,30 @@ use Symfony\Component\DependencyInjection\Exception\RuntimeException;
  */
 class GrumphpTasksExtension implements ExtensionInterface
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function load(ContainerBuilder $container)
-    {
-        if ($container->hasParameter('extra_tasks')) {
-            $tasks = $container->getParameter('tasks');
-
-            foreach ($container->getParameter('extra_tasks') as $name => $value) {
-                if (isset($tasks[$name])) {
-                    throw new RuntimeException(
-                      sprintf("Cannot override already defined task '%s' in 'extra_tasks'", $name)
-                    );
-                }
-
-                $tasks[$name] = $value;
-            }
-
-            $container->setParameter('tasks', $tasks);
+  /**
+  * {@inheritdoc}
+  */
+  public function load(ContainerBuilder $container): void
+  {
+    if ($container->hasParameter('extra_tasks')) {
+      $tasks = $container->getParameter('tasks');
+      foreach ($container->getParameter('extra_tasks') as $name => $value) {
+        if (array_key_exists($name, $tasks)) {
+          throw new RuntimeException("Cannot override already defined task '{$name}' in 'extra_tasks'.");
         }
+        $tasks[$name] = $value;
+      }
+      $container->setParameter('tasks', $tasks);
+    }
 
-      if ($container->hasParameter('skip_tasks')) {
-        $tasks = $container->getParameter('tasks');
+    if ($container->hasParameter('skip_tasks')) {
+      $tasks = $container->getParameter('tasks');
 
-        foreach ($container->getParameter('skip_tasks') as $name) {
-          unset($tasks[$name]);
-        }
-
-        $container->setParameter('tasks', $tasks);
+      foreach ($container->getParameter('skip_tasks') as $name) {
+        unset($tasks[$name]);
       }
 
+      $container->setParameter('tasks', $tasks);
     }
+  }
 }
