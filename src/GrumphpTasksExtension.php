5b03b5ab773398dfe8ef45707ad0261079d339de
williamroboly <78363364+williamroboly@users.noreply.github.com>
2021-02-25 21:48:35 -0500

    Update to ISED namespace.

--- src/GrumphpTasksExtension.php
+++ src/GrumphpTasksExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Robolo\ConventionsDrupal;
+namespace IsedIsde\ConventionsDrupal;
 
 use GrumPHP\Extension\ExtensionInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
